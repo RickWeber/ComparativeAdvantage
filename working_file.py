aad28631beb0f97868d012c07a1cf67a38cf71d9
Rick Weber <Rick.Weber05@gmail.com>
2020-09-16 13:04:50 -0400

    Solve some pandas problems

--- working_file.py
+++ working_file.py
@@ -9,6 +9,7 @@ import pandas as pd
 from mesa import Model
 from mesa import Agent
 from mesa.time import RandomActivation
+import copy
 
 
 class Market(Model):
@@ -16,6 +17,7 @@ class Market(Model):
         super().__init__()
         self.N = N
         self.K = K
+        self.possible_trades = self.gen_pos_trades(K)
         self.schedule = RandomActivation(self)
         self.history = pd.DataFrame({
             "partner1": [],
@@ -24,7 +26,6 @@ class Market(Model):
             "trades": []
         })
         self.trades_undertaken = 0
-        self.possible_trades = self.generates_trades(K)
         # create agents
         for i in range(N):
             a = BarterAgent(self.next_id(), self)
@@ -33,17 +34,22 @@ class Market(Model):
     def step(self):
         self.schedule.step()
 
-    def generate_possible_trades(self, K):
+    def gen_pos_trades(self, K):
         poss_trades = [(x, y) for x in range(K) for y in range(K) if x != y]
         poss_trades = pd.DataFrame(poss_trades)
-        poss_trades.rename(columns={'0': 'buy', '1': 'sell'}, inplace=True)
+        poss_trades.rename(columns={0: 'buy', 1: 'sell'}, inplace=True)
         return poss_trades
 
-
-class Exchange():
-    """A class to hold on to trade details"""
-    def __init__(self):
-        self.done = False
+    def gen_numeraire_trades(self, K):
+        """
+        Allow a version of the model where all trades involve "cash"
+        by designating good 0 as a numeraire.
+        """
+        poss_trades = [(x, y) for x in range(K) for y in range(K)
+                       if (x != y and (x == 0 or y == 0))]
+        poss_trades = pd.DataFrame(poss_trades)
+        poss_trades.rename(columns={0: 'buy', 1: 'sell'}, inplace=True)
+        return poss_trades
 
 
 class BarterAgent(Agent):
@@ -57,23 +63,30 @@ class BarterAgent(Agent):
         self.trades = np.ones(model.possible_trades.shape[0])
         self.ppf = np.random.randint(1, 4, model.K)
         # maximum prices are defined by the slope of the ppf
+        self.prices = np.array(
+            [self.ppf[y]/self.ppf[x]
+             for x in range(model.K)
+             for y in range(model.K)
+             if x != y]
+        )
         self.prices = [self.ppf[y]/self.ppf[x]
                        for (x, y) in model.possible_trades]
         self.endowment = np.random.randint(10, 20, model.K)
         u_params = np.random.randint(1, 4, model.K)
         self.u_params = u_params / u_params.sum()
-        self.trades_done = 0
+        self.trades_undertaken = 0
         self.cumulative_utility = 0
         self.memory = 10
+        self.age = 0
 
     def history(self):
         full_history = self.model.history
-        my_buys = full_history["partner1" == self]
-        my_sells = full_history["partner2" == self]
-        my_history = my_buys.row_join(my_sells)  # FIX
-        return my_history
+        my_buys = full_history["partner1"] == self
+        my_sells = full_history["partner2"] == self
+        return full_history[my_buys or my_sells]
 
     def step(self):
+        self.age += 1
         self.produce()
         partner = self.find_partner()
         self.trade(partner)
@@ -105,10 +118,10 @@ class BarterAgent(Agent):
     ### Work on stuff below ###
     def trade(self, partner, complexity=1):
         prob = self.trades / self.trades.sum()
-        index = np.random.randint(range(self.model.possible_trades.size[0]),
-                                  size=complexity,
-                                  replace=True,
-                                  p=prob)
+        index = np.random.choice(self.model.possible_trades.index,
+                                 size=complexity,
+                                 replace=True,
+                                 p=prob)
         trades = self.model.possible_trades.loc[index]
         prices = [(1, np.random.randint(1, self.prices[i])) for i in index]
         prices = np.array(prices)
@@ -125,13 +138,11 @@ class BarterAgent(Agent):
             return self
         else:
             hist_update = pd.DataFrame({
-                "initiator": [self],
-                "partner": [partner],
+                "partner1": [self],
+                "partner2": [partner],
                 "deal": [deal],
                 "trades": [trades]
             })
-            self.history = pd.concat([self.history, hist_update])
-            partner.history = pd.concat([partner.history, hist_update])
             self.model.history = pd.concat([self.model.history, hist_update])
             self.update()
             partner.update()
@@ -151,24 +162,27 @@ class BarterAgent(Agent):
         mutation = self.random_mutation()
         doppelganger = copy.deepcopy(self).mutate(mutation)
         if doppelganger.produce().utility() > baseline:
-            self.production.loc[mutation] += 1
+            self.mutate(mutation)
         return self
 
     def update(self):
         """Produce more of what I have comparative
         advantage in... Or, if I can't trade, whatever
         gives me greater expected utility."""
-        last_deal = self.history.tail(1)
-        if last_deal["initiator"] == self:
+        history = self.model.history["partner1" == self | "partner2" == self]
+        history = history.tail(self.memory)
+        last_deal = history.tail(1)
+        if last_deal["partner1"] == self:
             self.endowment += last_deal["deal"]
         else:
             self.endowment -= last_deal["deal"]
         self.trades_undertaken += 1
         # self.prod_plan[last_deal["deal"]] += 1
         # update prod_plan to make more of something I sold
-        make_more_of = last_deal["trades"][1][1]
-        self.production.loc[make_more_of] += 1
-        self.trades.loc[self.trades == last_deal["trades"]] += 1
+        choice = np.random.choice(last_deal["trades"], 1)
+        make_more_of = choice[1]
+        self.mutate("production", make_more_of, 1)
+        self.mutate("trades", last_deal["trades"], 1)
         return self
 
     def utility(self):
@@ -184,25 +198,21 @@ class BarterAgent(Agent):
 
     def reproduce(self, **mutations):
         baby = copy.deepcopy(self)
-        baby.history = pd.DataFrame({
-            "initiator": [],
-            "partner": [],
-            "deal": [],
-            "trades": []
-        })
         baby.trades_done = 0
         baby.cumulative_utility = 0
+        baby.age = 0
         if mutations:
             baby = baby.mutate(mutations)
         # add to schedule
         self.model.schedule.add(baby)
         return baby
 
+    # rebuild to work with lines 174/175
     def mutate(self, **mutations):
         for key, value in mutations:
             if key == "production":
                 self.production += value
-            if key == "trade":
+            if key == "trades":
                 self.trades += value
             if key == "u_params":
                 self.u_params += value
@@ -217,7 +227,7 @@ class BarterAgent(Agent):
         return self
 
     def random_mutation(self):
-        options = ["production", "trade", "u_params", "ppf", "endowment"]
+        options = ["production", "trades", "u_params", "ppf", "endowment"]
         prob = np.array([10, 10, 2, 2, 1])
         prob = prob / prob.sum()
         key = np.random.choice(options, 1, p=prob)
